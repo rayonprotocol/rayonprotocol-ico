bce3616616edc41a9c0b5b830783d6762f227748
eunwoo <aufcl4858@never.com>
2018-06-11 12:16:35 +0900

    Update configuration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,24 +1,20 @@
 const RayonCoinCrowdsale = artifacts.require('./RayonCoinCrowdsale.sol');
 const RayonCoin = artifacts.require('./RayonCoin.sol');
 
-module.exports = function (deployer, network, accounts) {
-    const startTime = Math.round((new Date(Date.now() + 86400000).getTime()) / 1000); // Yesterday
-    const endTime = Math.round((new Date().getTime() + (86400000 * 20)) / 1000); // Today + 20 days
-    const rate = 5;
-    // const wallet = accounts[1];
-    const wallet = "0xCA19ddbca9ad926e6DEAd313F850f04449360b89";
-    return deployer
-        .then(() => {
-            return deployer.deploy(RayonCoin);
-        })
-        .then(() => {
-            return deployer.deploy(
-                RayonCoinCrowdsale,
-                startTime,
-                endTime,
-                rate,
-                wallet,
-                RayonCoin.address
-            );
-        });
+module.exports = function(deployer, network, accounts) {
+  return deployer
+    .then(() => {
+      return deployer.deploy(RayonCoin);
+    })
+    .then(() => {
+      const latest = web3.eth.getBlock('latest');
+      console.log('latest', latest.timeStamp);
+      const startTime = Math.round(new Date(Date.now() + 86400000).getTime() / 1000);
+      const endTime = Math.round((new Date().getTime() + 86400000 * 20) / 1000); // Today + 20 days
+      const rate = 5;
+      // const wallet = accounts[0];
+      const wallet = '0xCA19ddbca9ad926e6DEAd313F850f04449360b89';
+      console.log('HD wallet accounts', accounts);
+      return deployer.deploy(RayonCoinCrowdsale, startTime, endTime, rate, wallet, RayonCoin.address);
+    });
 };
--- truffle.js
+++ truffle.js
@@ -1,20 +1,24 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var infura_apikey = "JIA5eiNFvLhAStdxeTvb"; // Either use this key or get yours at https://infura.io/signup. It's free.
-var mnemonic = "zoo you win van use two sun rib pig own now ten";
+var HDWalletProvider = require('truffle-hdwallet-provider');
+var infura_apikey = 'JIA5eiNFvLhAStdxeTvb'; // Either use this key or get yours at https://infura.io/signup. It's free.
+var mnemonic = 'zoo you win van use two sun rib pig own now ten';
+var devMnemonic = 'rhythm vicious awful truck hint boring scale debris embark forest decline salad';
 
 module.exports = {
-    networks: {
-        development: {
-            host: "localhost",
-            port: 8545,
-            network_id: "*" // Match any network id
-        },
-        ropsten: {
-            provider: function () {
-                return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/" + infura_apikey);
-            },
-            network_id: 3,
-            gas: 5000000
-        }
-    }
+  networks: {
+    development: {
+      provider: function() {
+        return new HDWalletProvider(devMnemonic, 'http://localhost:7545');
+      },
+      gas: 90000000000,
+      gasPrice: 1,
+      network_id: '*',
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/' + infura_apikey);
+      },
+      network_id: 3,
+      gas: 4600000,
+    },
+  },
 };
