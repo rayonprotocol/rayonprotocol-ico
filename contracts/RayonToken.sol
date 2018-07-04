1f4f5f68356bd319a84c5ab5c84b2dcc38497077
knilnissim <knilnissim@gmail.com>
2018-07-04 19:09:03 +0900

    extract only required contracts from "zeppelin-solidity" module.

--- contracts/RayonToken.sol
+++ contracts/RayonToken.sol
@@ -1,9 +1,9 @@
 
 pragma solidity ^0.4.23;
 
-import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "./token/MintableToken.sol";
 
-contract RayonCoin is MintableToken {
+contract RayonToken is MintableToken {
     string public name = "RAYON COIN";
     string public symbol = "RYN";
     uint8 public decimals = 18;
