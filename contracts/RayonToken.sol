8ab97bfc17f9eb7e1f5d94aa29f6b61719bd95be
knil nissim <knilnissim@gmail.com>
2018-07-17 18:50:07 +0900

    Add BurnableToken that can be irreversibly burned (destroyed)

--- contracts/RayonToken.sol
+++ contracts/RayonToken.sol
@@ -2,10 +2,11 @@
 pragma solidity ^0.4.23;
 
 import "./token/MintableToken.sol";
+import "./token/BurnableToken.sol";
 import "./token/TransferLimitedToken.sol";
 
 
-contract RayonToken is MintableToken, TransferLimitedToken {
+contract RayonToken is MintableToken, TransferLimitedToken, BurnableToken {
     string public name = "RAYON";
     string public symbol = "RYN";
     uint8 public decimals = 18;
