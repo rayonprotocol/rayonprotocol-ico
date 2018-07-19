35d4a0f4270903579285a360e1c568e353e5749e
knil nissim <knilnissim@gmail.com>
2018-07-19 18:38:24 +0900

    Fix typo

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,13 +5,15 @@ const BigNumber = web3.BigNumber;
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(RayonToken);
 
-  // these arguments are only used for development thus does not reflect whitepaper
+  // these arguments are only used for development thus do not reflect whitepaper
   const openingTime = Math.round(new Date(Date.now() + 200000).getTime() / 1000);
   const closingTime = Math.round((new Date().getTime() + 86400000 * 20) / 1000); // Today + 20 days
   const rate = 500;
   const [wallet] = accounts;
+  const token = RayonToken.address;
   const cap = new BigNumber(web3.toWei('3000', 'ether'));
+
   await deployer.deploy(RayonTokenCrowdsale,
-    rate, wallet, RayonToken.address, cap, openingTime, closingTime
+    rate, wallet, token, cap, openingTime, closingTime
   );
 };
