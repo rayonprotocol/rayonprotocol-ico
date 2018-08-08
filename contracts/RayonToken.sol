a184c6f77dc30c9ae6e618d4d2d8fac4450d472b
knil nissim <knilnissim@gmail.com>
2018-08-08 11:30:46 +0900

    Add HasNoEther to RayonToken to block incoming ether

--- contracts/RayonToken.sol
+++ contracts/RayonToken.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.4.23;
 
 import "openzeppelin-solidity/contracts/ownership/Claimable.sol";
+import "openzeppelin-solidity/contracts/ownership/HasNoEther.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/CappedToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/BurnableToken.sol";
 import "./token/IndividuallyPausableToken.sol";
 
-contract RayonToken is Claimable, CappedToken, PausableToken, IndividuallyPausableToken, BurnableToken {
+contract RayonToken is Claimable, HasNoEther, CappedToken, PausableToken, IndividuallyPausableToken, BurnableToken {
     string public name = "RAYON";
     string public symbol = "RYN";
     uint8 public decimals = 18;
