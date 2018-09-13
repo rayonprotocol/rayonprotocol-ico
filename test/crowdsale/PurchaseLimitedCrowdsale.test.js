c9f1c35c3adb410cda842a3d24f3c6c11b9147b2
kyungjeon <kyungjeon@finda.co.kr>
2018-09-13 16:55:55 +0900

    RYN-63 Review and improve PurchaseLimitedCrowdsale and PurchaseLimitedCrowdsale.test

--- test/crowdsale/PurchaseLimitedCrowdsale.test.js
+++ test/crowdsale/PurchaseLimitedCrowdsale.test.js
@@ -10,62 +10,70 @@ require('chai')
 const ether = (n) => new BigNumber(web3.toWei(n, 'ether'));
 
 contract('PurchaseLimitedCrowdsale', function (accounts) {
-  const [owner, purchaser] = accounts;
+  const [owner, purchaser1, purchaser2] = accounts;
   const rate = 500;
-  const wallet = owner;
   const tokenSupply = new BigNumber('1e25');
-  const value = ether(4);
+
   const minimumLimit = ether(2);
   const maximumLimit = ether(10);
+  const possibleValue = ether(4);
   const excessValue = maximumLimit.add(ether(1));
   const underValue = minimumLimit.sub(ether(1));
 
   let crowdsale;
   let token;
+
   beforeEach(async function () {
     token = await MintableToken.new();
-    crowdsale = await PurchaseLimitedCrowdsale.new(
-      minimumLimit, maximumLimit, rate, wallet, token.address,
-    );
+    crowdsale = await PurchaseLimitedCrowdsale.new(minimumLimit, maximumLimit, rate, owner, token.address);
     await token.mint(crowdsale.address, tokenSupply);
   });
 
-  describe('total purchase amount', async function () {
+  describe('1. TOTAL PURCHASE AMOUNT', async function () {
     it('accumulates purchase amount', async function () {
-      const raisedWeiBeforePurchase = await crowdsale.contributions(purchaser);
+      const raisedWeiBeforePurchase = await crowdsale.contributions(purchaser1);
       raisedWeiBeforePurchase.should.be.bignumber.equal(0);
 
-      await crowdsale.sendTransaction({ value, from: purchaser }).should.be.fulfilled;
+      await crowdsale.sendTransaction({ value: possibleValue, from: purchaser1}).should.be.fulfilled;
 
-      const raisedWeiAfterPurchase = await crowdsale.contributions(purchaser);
-      raisedWeiAfterPurchase.should.be.bignumber.equal(value);
+      const raisedWeiAfterPurchase = await crowdsale.contributions(purchaser1);
+      raisedWeiAfterPurchase.should.be.bignumber.equal(possibleValue);
 
-      await crowdsale.sendTransaction({ value, from: purchaser }).should.be.fulfilled;
+      await crowdsale.sendTransaction({ value: possibleValue, from: purchaser1}).should.be.fulfilled;
 
-      const raisedWeiAfterSecondPurchase = await crowdsale.contributions(purchaser);
-      raisedWeiAfterSecondPurchase.should.be.bignumber.equal(value.times(2));
+      const raisedWeiAfterSecondPurchase = await crowdsale.contributions(purchaser1);
+      raisedWeiAfterSecondPurchase.should.be.bignumber.equal(possibleValue.times(2));
     });
   });
 
-  describe('purchase limit', async function () {
+  describe('2. PURCHASE LIMIT', async function () {
     it('accepts the purchase by the minimum amount', async function () {
-      await crowdsale.sendTransaction({ value: minimumLimit }).should.be.fulfilled;
+      await crowdsale.sendTransaction({ value: minimumLimit, from: purchaser1}).should.be.fulfilled;
     });
 
     it('accepts the purchase by the maximum amount', async function () {
-      await crowdsale.sendTransaction({ value: maximumLimit }).should.be.fulfilled;
+      await crowdsale.sendTransaction({ value: maximumLimit, from: purchaser1}).should.be.fulfilled;
     });
 
     it('accepts the purchase for amount between minimumLimit and maximumLimit', async function () {
-      await crowdsale.sendTransaction({ value }).should.be.fulfilled;
+      await crowdsale.sendTransaction({ value: possibleValue, from: purchaser1}).should.be.fulfilled;
     });
 
     it('rejects the purchase under minimum amount', async function () {
-      await crowdsale.sendTransaction({ value: underValue }).should.be.rejectedWith(/revert/);
+      await crowdsale.sendTransaction({ value: underValue, from: purchaser1}).should.be.rejectedWith(/revert/);
     });
 
     it('rejects the purchase excessing maximum amount', async function () {
-      await crowdsale.sendTransaction({ value: excessValue }).should.be.rejectedWith(/revert/);
+      await crowdsale.sendTransaction({ value: excessValue, from: purchaser1}).should.be.rejectedWith(/revert/);
+      await crowdsale.sendTransaction({ value: minimumLimit, from: purchaser1}).should.be.fulfilled;
+      await crowdsale.sendTransaction({ value: maximumLimit, from: purchaser1}).should.be.rejectedWith(/revert/);
+    });
+
+    it('accept the purchase by a different purchaser', async function () {
+      await crowdsale.sendTransaction({ value: maximumLimit, from: purchaser1}).should.be.fulfilled;
+      await crowdsale.sendTransaction({ value: possibleValue, from: purchaser1}).should.be.rejectedWith(/revert/);
+      // different purchaser
+      await crowdsale.sendTransaction({ value: possibleValue, from: purchaser2}).should.be.fulfilled;
     });
   });
 });
